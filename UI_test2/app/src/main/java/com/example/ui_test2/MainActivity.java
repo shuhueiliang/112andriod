2c7dda6036cac93b0452587308f6458421240892
shuhueiliang <a111222032@mail.shu.edu.tw>
2024-03-22 14:59:52 +0800

    2024-03-22

---
+++ UI_test2/app/src/main/java/com/example/ui_test2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ui_test2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.ui_test2.DisplayStringActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+
+    public void sendString(View view){
+        EditText edtInput = (EditText) findViewById(R.id.edtInput);
+        String string2Send = edtInput.getText().toString();
+
+        Intent intent = new Intent( this, DisplayStringActivity.class);
+        intent.putExtra( "userInput", string2Send);
+        startActivity(intent);
+    }
+}
